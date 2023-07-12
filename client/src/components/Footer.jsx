de9c76c8b4d07b00b5fe2b4c759b22d75bbf6d72
Christopher Jin <chrisjin1@gmail.com>
2023-07-12 17:30:45 -0400

    initial landing page components from daisy

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+  <div>
+    <p>Christopher Jin © 2023 - All rights reserved</p>
+  </div>
+</footer>
+  )
+}
+
+export default Footer
