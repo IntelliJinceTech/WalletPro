6ff792690689ededa59d32ed88c07d474169adbc
Christopher Jin <chrisjin1@gmail.com>
2023-07-16 14:13:39 -0400

    adding margin to separate credit card rows on dashboard

--- client/src/pages/Dashboard/Wallet.jsx
+++ client/src/pages/Dashboard/Wallet.jsx
@@ -3,17 +3,19 @@ import Categories from './CreditCardCategories'
 
 const Wallet = () => {
   return (
-
-      <div className="flex flex-col lg:flex-row px-3 mx-auto">
+    <div>
+      <div className="flex flex-col lg:flex-row px-3 mx-auto mb-5">
           <div className="grid grid-cols-3 gap-3 max-h-1/2">
             <CreditCard />
             <Categories />
           </div>
-          
-          {/* <CreditCard  />
-          <div className="border-solid border-2 border-sky-300 col-span-2">Categories</div> */}
-
-
+      </div>
+      <div className="flex flex-col lg:flex-row px-3 mx-auto mb-5">
+          <div className="grid grid-cols-3 gap-3 max-h-1/2">
+            <CreditCard />
+            <Categories />
+          </div>
+      </div>
       </div>
 
   )
