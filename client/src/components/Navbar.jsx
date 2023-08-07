846c736eab51d0a2ee7d1675025857fd5ee88a70
Christopher Jin <chrisjin1@gmail.com>
2023-08-07 12:18:00 -0400

    navbar and hero section changes to tailwind

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,82 +1,229 @@
-import { CgProfile } from 'react-icons/cg'
-import { themeChange } from 'theme-change'
-import { useEffect } from 'react'
-import { useAuthContext } from '../context/AuthContext/authContext'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const Navbar = (props) => {
-  const { isAuthenticated, login, logout } = useAuthContext()
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
-  useEffect(() => {
-    themeChange(false)
-  }, [])
-
-  const toggleLogin = () => {
-    if (isAuthenticated) {
-      logout()
-    } else {
-      login()
-    }
-  }
-
+const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">WalletPro</a>
-      </div>
-      <div className="flex-none ">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <div className="flex items-stretch p-0">
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost rounded-btn">
-                  <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-5 w-5 stroke-current">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-                    ></path>
-                  </svg>
-                  Theme
-                </label>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <div>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                    <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
+                      Dashboard
+                    </a>
+                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                      Team
+                    </a>
+                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                      Projects
+                    </a>
+                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                      Calendar
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex items-center">
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                              Your Profile
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                              Settings
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                              Sign out
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </div>
+              <div className="-mr-2 flex sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
+                </Disclosure.Button>
               </div>
             </div>
+          </div>
 
-            {/* <select data-choose-theme className="select select-primary w-full max-w-xs rounded p-2">
-              <option disabled selected >
-                Select Theme
-              </option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-              <option value="corporate">Corporate</option>
-              <option value="business">Business</option>
-            </select> */}
-          </li>
-        </ul>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <CgProfile className=" text-lg min-h-full min-w-full" />
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+              <Disclosure.Button as="a" href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">
+                Dashboard
+              </Disclosure.Button>
+              <Disclosure.Button as="a" href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                Team
+              </Disclosure.Button>
+              <Disclosure.Button as="a" href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                Projects
+              </Disclosure.Button>
+              <Disclosure.Button as="a" href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                Calendar
+              </Disclosure.Button>
             </div>
-          </label>
-          <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-            <li>
-              <a className="justify-between">
-                Profile
-                {/* <span className="badge">New</span> */}
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a onClick={toggleLogin}>{isAuthenticated ? `Logout` : `Login`}</a>
-            </li>
-          </ul>
+            <div className="border-t border-gray-700 pb-3 pt-4">
+              <div className="flex items-center px-5">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium text-white">Tom Cook</div>
+                  <div className="text-sm font-medium text-gray-400">tom@example.com</div>
+                </div>
+                <button
+                  type="button"
+                  className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-3 space-y-1 px-2">
+                <Disclosure.Button as="a" href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+                  Your Profile
+                </Disclosure.Button>
+                <Disclosure.Button as="a" href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+                  Settings
+                </Disclosure.Button>
+                <Disclosure.Button as="a" href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+                  Sign out
+                </Disclosure.Button>
+              </div>
+            </div>
+          </Disclosure.Panel>
         </div>
-      </div>
-    </div>
+      )}
+    </Disclosure>
   )
 }
+
+// import { CgProfile } from 'react-icons/cg'
+// import { themeChange } from 'theme-change'
+// import { useEffect } from 'react'
+// import { useAuthContext } from '../context/AuthContext/authContext'
+
+// const Navbar = (props) => {
+//   const { isAuthenticated, login, logout } = useAuthContext()
+
+//   useEffect(() => {
+//     themeChange(false)
+//   }, [])
+
+//   const toggleLogin = () => {
+//     if (isAuthenticated) {
+//       logout()
+//     } else {
+//       login()
+//     }
+//   }
+
+//   return (
+
+//     <div className="navbar bg-base-100">
+//       <div className="flex-1">
+//         <a className="btn btn-ghost normal-case text-xl">WalletPro</a>
+//       </div>
+//       <div className="flex-none ">
+//         <ul className="menu menu-horizontal px-1">
+//           <li>
+//             <div className="flex items-stretch p-0">
+//               <div className="dropdown dropdown-end">
+//                 <label tabIndex={0} className="btn btn-ghost rounded-btn">
+//                   <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-5 w-5 stroke-current">
+//                     <path
+//                       strokeLinecap="round"
+//                       strokeLinejoin="round"
+//                       strokeWidth="2"
+//                       d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+//                     ></path>
+//                   </svg>
+//                   Theme
+//                 </label>
+//               </div>
+//             </div>
+//           </li>
+//         </ul>
+//         <div className="dropdown dropdown-end">
+//           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+//             <div className="w-10 rounded-full">
+//               <CgProfile className=" text-lg min-h-full min-w-full" />
+//             </div>
+//           </label>
+//           <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+//             <li>
+//               <a className="justify-between">
+//                 Profile
+//                 {/* <span className="badge">New</span> */}
+//               </a>
+//             </li>
+//             <li>
+//               <a>Settings</a>
+//             </li>
+//             <li>
+//               <a onClick={toggleLogin}>{isAuthenticated ? `Logout` : `Login`}</a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default Navbar
