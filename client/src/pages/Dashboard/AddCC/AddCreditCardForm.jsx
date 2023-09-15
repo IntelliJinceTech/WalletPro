66557a1506b112e0cbcb7da0850b1d875c3e0a79
Christopher Jin <chrisjin1@gmail.com>
2023-09-15 00:33:52 -0400

    conditional rendering on the add credit card form

--- client/src/pages/Dashboard/AddCC/AddCreditCardForm.jsx
+++ client/src/pages/Dashboard/AddCC/AddCreditCardForm.jsx
@@ -5,52 +5,58 @@ import { ErrorMessage } from '@hookform/error-message'
 import CategorySelection from './CategorySelection'
 
 const AddCreditCardForm = () => {
+  const [cardType, setCardType] = useState(null)
+  const numberBestCategories = 3
   const methods = useForm({
     criteriaMode: 'all', // all errors from each field will be gathered
   })
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = methods
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data, e) => console.log(data, e)
 
   const creditCardTypes = [
     { id: 'ccPoints', type: 'Points Multiplier', reference: 'points' },
     { id: 'ccPercentage', type: 'Percentage', reference: 'percentage' },
   ]
 
-  const [cardType, setCardType] = useState(null)
-  const watchType = watch('categoryType')
-
   useEffect(() => {
     console.log(cardType)
   }, [cardType])
 
+  const handleRadioChange = (e) => {
+    console.log(e)
+  }
+
   return (
     <FormProvider {...methods}>
-      <form method="post" className=" w-full max-w-lg place-content-center" onSubmit={methods.handleSubmit(onSubmit)}>
+      <form className=" w-full max-w-lg place-content-center" onSubmit={methods.handleSubmit(onSubmit)}>
         <h2 className="text-base font-semibold leading-7 text-gray-900">General Info</h2>
         <div className="rounded-md px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
-          <label htmlFor="bank" className="block text-xs font-medium text-gray-900">
+          <label htmlFor="bankName" className="block text-xs font-medium text-gray-900">
             Credit Card Bank
           </label>
           <input
-            {...methods.register('bank', {
-              required: 'This input is required',
-              pattern: {
-                value: /[a-zA-Z]+$/,
-                message: 'Alphabetic input only',
-              },
-            })}
+            {...methods.register(
+              'bankName'
+              // {
+              //   required: 'This input is required',
+              //   pattern: {
+              //     value: /[a-zA-Z]+$/,
+              //     message: 'Alphabetic input only',
+              //   },
+              // }
+            )}
+            id="bankName"
             type="text"
-            name="bank"
+            name="bankName"
             className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
             placeholder="Example: Chase"
           />
         </div>
-        <ErrorMessage
+        {/* <ErrorMessage
           errors={errors}
           name="bank"
           render={({ messages }) =>
@@ -61,56 +67,43 @@ const AddCreditCardForm = () => {
               </p>
             ))
           }
-        />
+        /> */}
         <div className="rounded-md px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600 mt-1">
           <label htmlFor="ccName" className="block text-xs font-medium text-gray-900">
             Name
           </label>
           <input
-            {...methods.register('bank', {
-              required: 'This input is required',
-              pattern: {
-                value: /[a-zA-Z]+$/,
-                message: 'Alphabetic input only',
-              },
-            })}
+            id="ccName"
+            {...methods.register('ccName')}
             type="text"
             name="ccName"
             className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
             placeholder="Example: Sapphire Preferred"
           />
         </div>
-        <ErrorMessage
-          errors={errors}
-          name="ccName"
-          render={({ messages }) =>
-            messages &&
-            Object.entries(messages).map(([type, message]) => (
-              <p className="text-red-500 before:inline before:content-['⚠\00a0']" key={type}>
-                {message}
-              </p>
-            ))
-          }
-        />
         <div className="rounded-md px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600 mt-1">
           <label htmlFor="ccNetwork" className="block text-xs font-medium text-gray-900">
             Network
           </label>
           <input
-            {...methods.register('bank', {
-              required: 'This input is required',
-              pattern: {
-                value: /[a-zA-Z]+$/,
-                message: 'Alphabetic input only',
-              },
-            })}
+            {...methods.register(
+              'ccNetwork'
+              // {
+              //   required: 'This input is required',
+              //   pattern: {
+              //     value: /[a-zA-Z]+$/,
+              //     message: 'Alphabetic input only',
+              //   },
+              // }
+            )}
+            id="ccNetwork"
             type="text"
             name="ccNetwork"
             className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
             placeholder="Example: VISA"
           />
         </div>
-        <ErrorMessage
+        {/* <ErrorMessage
           errors={errors}
           name="network"
           render={({ messages }) =>
@@ -121,7 +114,7 @@ const AddCreditCardForm = () => {
               </p>
             ))
           }
-        />
+        /> */}
         <div className="mt-2">
           <label className="text-base font-semibold text-gray-900">Reward/Incentive Type</label>
           <p className="text-sm text-gray-500">What kind of credit card incentives does it deploy?</p>
@@ -132,14 +125,14 @@ const AddCreditCardForm = () => {
                 <div key={creditCardType.id} className="flex items-center">
                   <input
                     id={creditCardType.id}
-                    name="categoryType"
+                    name="rewardType"
                     type="radio"
                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    value={creditCardType.type}
-                    {...methods.register('categoryType', {
-                      required: 'This input is required',
-                    })}
-                    onChange={(e) => setCardType(creditCardType.reference)}
+                    defaultChecked={creditCardType.id === `ccPoints`}
+                    value={creditCardType.reference}
+                    // checked={creditCardType.
+                    onChange={handleRadioChange}
+                    {...register('rewardType', { required: 'This input is required' })}
                   />
                   <label htmlFor={creditCardType.id} className="ml-3 block text-sm font-medium leading-6 text-gray-900">
                     {creditCardType.type}
@@ -149,19 +142,15 @@ const AddCreditCardForm = () => {
             </div>
           </fieldset>
         </div>
-        <div>
-          <div>
-            <CategorySelection creditCardIncentiveType={cardType} />
+        {/* <div>
+          <div className="mt-3">
+            <label className="text-base font-semibold text-gray-900">Best 3 Categories</label>
+            <p className="text-sm text-gray-500">Setup the best categories that this credit card covers</p>
+            {cardType && [...Array(numberBestCategories)].map((_, i) => <CategorySelection key={i} creditCardIncentiveType={cardType} />)}
           </div>
-          <button
-            type="button"
-            className="rounded bg-green-600 px-2 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full my-2"
-          >
-            Add Category
-          </button>
-        </div>
+        </div> */}
         <button
-          className="rounded bg-indigo-600 px-2 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full"
+          className="mt-5 rounded bg-indigo-600 px-2 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full"
           type="submit"
         >
           Add Credit Card
--- client/src/pages/Dashboard/AddCC/CategorySelection.jsx
+++ client/src/pages/Dashboard/AddCC/CategorySelection.jsx
@@ -1,41 +1,41 @@
 import { Fragment, useState } from 'react'
+import { useFormContext, useFieldArray } from 'react-hook-form'
 import { PiPercentLight } from 'react-icons/pi'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join('  ')
 }
 
-const allCategories = [
-  { id: 1, category: 'Groceries' },
-  { id: 2, category: 'Travel' },
-  { id: 3, category: 'Dining' },
-  { id: 4, category: 'Gas' },
-  { id: 5, category: 'Other' },
-]
 const CategorySelection = ({ creditCardIncentiveType }) => {
-  const [selected, setSelected] = useState(allCategories[0])
+  const [selected, setSelected] = useState(null)
+  const { register, control } = useFormContext()
+  const { fields, append, remove } = useFieldArray({
+    name: 'categories',
+  })
+
   return (
     <div className="flex justify-around">
       <div className="flex items-center gap-x-1">
-        <label htmlFor="ccCategory" className="inline-block text-sm font-medium leading-6 text-gray-900 mr-3 pt-1">
+        <label htmlFor="ccCategory" className="sr-only">
           Category
         </label>
         <select
           id="ccCategory"
           name="ccCategory"
           className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          defaultValue=""
+          {...register(`categories.categoryType`)}
         >
           <option value="groceries">Groceries</option>
-          <option value="Travel">Travel</option>
-          <option value="Dining">Dining</option>
-          <option value="Gas">Gas</option>
-          <option value="Other">Other</option>
+          <option value="travel">Travel</option>
+          <option value="travel">Purchases through Portal</option>
+          <option value="dining">Dining</option>
+          <option value="gas">Gas</option>
+          <option value="other">Other</option>
         </select>
       </div>
       {creditCardIncentiveType === 'percentage' && (
         <div>
-          <label htmlFor="percent" className="sr-only">
+          <label htmlFor="categoryPercent" className="sr-only">
             Percent
           </label>
           <div className="relative mt-2 rounded-md shadow-sm">
@@ -45,15 +45,16 @@ const CategorySelection = ({ creditCardIncentiveType }) => {
             <input
               type="number"
               name="percent"
-              id="percent"
+              id="categoryPercent"
               className="block w-[60%] rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="5 = 5%"
+              {...register(`categories.${selected}Percentage`)}
             />
           </div>
         </div>
       )}
       {creditCardIncentiveType === 'points' && (
-        <div>
+        <div className="max-w-[25%]">
           <label htmlFor="points" className="sr-only">
             Percent
           </label>
@@ -67,12 +68,25 @@ const CategorySelection = ({ creditCardIncentiveType }) => {
               type="number"
               name="points"
               id="points"
-              className="block w-[60%] rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block rounded-md border-0 w-full py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
               placeholder="5 = 5x"
+              {...register(`${selected}PointsMultiplier`, { min: 0, max: 99 })}
             />
           </div>
         </div>
       )}
+      {/* <button
+        type="button"
+        className="block rounded-md border-0 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-pink-600 content-center"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-5">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+          />
+        </svg>
+      </button> */}
     </div>
   )
 }
